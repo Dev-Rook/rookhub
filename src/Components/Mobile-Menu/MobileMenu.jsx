3a7270f1675d62a93c3799e1ce56b3fe34d36158
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-08-05 19:55:44 -0500

    Swapped navlink text for Icons

--- src/Components/Mobile-Menu/MobileMenu.jsx
+++ src/Components/Mobile-Menu/MobileMenu.jsx
@@ -7,27 +7,27 @@ const MobileMenu = ({showMenu}) => {
     <ul className={showMenu ? "Mobile-Navlink-Container Show-Menu" : "Mobile-Navlink-Container"}>
         <li className="Mobile-Navlink-Items">
             <a href="#" className="Mobile-Navlink">
-                Home
+                <img src={require("../../Assets/Icons/Home-Icon.png")} alt="" className="NavIcon" />
             </a>
         </li>
         <li className="Mobile-Navlink-Items">
             <a href="#" className="Mobile-Navlink">
-                Portfolio
+                <img src={require("../../Assets/Icons/Portfolio-Icon.png")} alt="" className="NavIcon" />
             </a>
         </li>
         <li className="Mobile-Navlink-Items">
             <a href="#" className="Mobile-Navlink">
-                About
+                <img src={require("../../Assets/Icons/About-Icon.png")} alt="" className="NavIcon" />
             </a>
         </li>
         <li className="Mobile-Navlink-Items">
             <a href="#" className="Mobile-Navlink">
-                Contact
+                <img src={require("../../Assets/Icons/Dev-Logs-Icon.png")} alt="" className="NavIcon" />
             </a>
         </li>
         <li className="Mobile-Navlink-Items">
             <a href="#" className="Mobile-Navlink DevLogs">
-                Dev Logs
+                <img src={require("../../Assets/Icons/Contact-Icon.png")} alt="" className="NavIcon" />
             </a>
         </li>
     </ul>
