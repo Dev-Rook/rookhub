315c8f70883d73429e8efa75bdfc955b5b0e1d62
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-08-05 23:14:47 -0500

    Website fully responsive

--- src/Components/Page-Components/Intro/Intro.jsx
+++ src/Components/Page-Components/Intro/Intro.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
-
 import "../_Global-Page-Styles/GlobalPageStyles.css"
 
 import ContactButton from '../Main-Button/ContactButton'
 
 const Intro = () => {
   return (
-    <div className="Page-Section">
+    <div className="Page-Section Intro-Section">
         <div className="Header-Container">
             <h1 className="Header-Text">
                 Hi, I'm <br /> Daniel <span className='Red-Letter'>R</span>ook
             </h1>
-
             <p className="Job-Title">
                 <span className='Red-Letter'>-</span> Frontend Web Developer
-            </p>
-            
+            </p>    
             <ContactButton />
         </div>
 
